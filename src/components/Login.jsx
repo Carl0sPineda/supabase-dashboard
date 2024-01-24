72a9232e37ffc21b14e5845f4745f57094662e9d
Carl0sPineda <carlos7712p@gmail.com>
2024-01-23 20:26:29 -0600

    login page styles added

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import toast from "react-hot-toast";
 import supabase from "../lib/Supabase";
+import login_img from "../assets/login-img.avif";
 
 const Login = () => {
   const [email, setEmail] = useState("");
@@ -15,41 +16,85 @@ const Login = () => {
         password,
       });
       if (error) {
-        toast.error(
-          "Credenciales incorrectas. Verifica tu email o contraseña."
-        );
+        toast.error("Credenciales incorrectas!!");
       } else {
         toast.success("Inicio de sesión con éxito!");
       }
     } catch (error) {
-      toast.error("Credenciales incorrectas. Verifica tu email o contraseña.");
+      toast.error("Credenciales incorrectas!!");
     }
   };
 
   return (
-    <form onSubmit={handleLogin}>
-      <label>
-        Correo:{" "}
-        <input
-          className="border border-gray-300 rounded-md"
-          type="email"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-        />
-      </label>
-      <label>
-        Contraseña:{" "}
-        <input
-          className="border border-gray-300 rounded-md"
-          type="password"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-        />
-      </label>
-      <button type="submit" className="bg-black text-white px-5 py-2">
-        Inicia sesión
-      </button>
-    </form>
+    <>
+      <div className="flex justify-center items-center h-screen">
+        {/* Left: Image */}
+        <div className="w-1/2 h-screen hidden lg:block">
+          <img
+            src={login_img}
+            alt="Placeholder Image"
+            className="object-cover w-full h-full"
+          />
+        </div>
+        {/* Right: Login Form */}
+        <div className="lg:p-36 md:p-52 sm:20 p-8 w-full lg:w-1/2">
+          <h2 className="text-center text-2xl sm:text-3xl font-bold leading-9 tracking-tight text-gray-900">
+            Inicio de sesión
+          </h2>
+          <form className="space-y-6 mt-5">
+            <div>
+              <label
+                htmlFor="email"
+                className="block text-sm font-medium leading-6 text-gray-900"
+              >
+                Correo
+              </label>
+              <div className="mt-2">
+                <input
+                  id="email"
+                  type="email"
+                  value={email}
+                  placeholder="andres@gmail.com"
+                  className="w-full border border-gray-300 rounded-md py-2 px-3 focus:outline-none focus:border-blue-500"
+                  onChange={(e) => setEmail(e.target.value)}
+                  autoComplete="on"
+                />
+              </div>
+            </div>
+            <div>
+              <div className="flex items-center justify-between">
+                <label
+                  name="password"
+                  htmlFor="password"
+                  className="block text-sm font-medium leading-6 text-gray-900"
+                >
+                  Contraseña
+                </label>
+              </div>
+              <div className="mt-2">
+                <input
+                  id="password"
+                  type="password"
+                  value={password}
+                  placeholder="********"
+                  className="w-full border border-gray-300 rounded-md py-2 px-3 focus:outline-none focus:border-blue-500"
+                  onChange={(e) => setPassword(e.target.value)}
+                  autoComplete="on"
+                />
+              </div>
+            </div>
+            <div>
+              <button
+                onClick={handleLogin}
+                className="flex w-full justify-center rounded-md bg-slate-800 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm"
+              >
+                Ingresar
+              </button>
+            </div>
+          </form>
+        </div>
+      </div>
+    </>
   );
 };
 
