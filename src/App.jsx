aff02f9bb537e1cd378fd10c573466bea97497c2
Carl0sPineda <carlos7712p@gmail.com>
2024-01-24 10:09:17 -0600

    protected hooks and routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,30 @@
-import Dashboard from "./components/Dashboard";
-import Login from "./components/Login";
 import { Toaster } from "react-hot-toast";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { AuthProvider } from "./hooks/useAuth";
+import Dashboard from "./components/Dashboard";
+import Login from "./components/Login";
+import ProtectedRoute from "./components/ProtectedRoute";
+import NotFound from "./pages/NotFound";
 import "./index.css";
 
 const App = () => {
   return (
     <>
       <Router>
-        <Routes>
-          <Route path="/" element={<Login />} />
-          <Route path="/dashboard" element={<Dashboard />} />
-        </Routes>
+        <AuthProvider>
+          <Routes>
+            <Route path="/" element={<Login />} />
+            <Route
+              path="/dashboard"
+              element={
+                <ProtectedRoute>
+                  <Dashboard />
+                </ProtectedRoute>
+              }
+            />
+            <Route path="*" element={<NotFound />} />
+          </Routes>
+        </AuthProvider>
       </Router>
       <Toaster position="top-right" reverseOrder={false} />
     </>
