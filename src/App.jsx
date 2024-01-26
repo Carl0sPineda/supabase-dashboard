5415658d29675a88d5241e80bb175f455034b8e9
Carl0sPineda <carlos7712p@gmail.com>
2024-01-26 16:45:13 -0600

    sidebard functional and protected dashboard routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,31 @@
 import { Toaster } from "react-hot-toast";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { AuthProvider } from "./hooks/useAuth";
-import Dashboard from "./components/Dashboard";
 import Login from "./components/Login";
 import ProtectedRoute from "./components/ProtectedRoute";
 import NotFound from "./pages/NotFound";
+import Dashboard from "./components/Dashboard";
 import "./index.css";
 
 const App = () => {
   return (
-    <>
-      <Router>
-        <AuthProvider>
-          <Routes>
-            <Route path="/" element={<Login />} />
-            <Route
-              path="/dashboard"
-              element={
-                <ProtectedRoute>
-                  <Dashboard />
-                </ProtectedRoute>
-              }
-            />
-            <Route path="*" element={<NotFound />} />
-          </Routes>
-        </AuthProvider>
-      </Router>
+    <Router>
+      <AuthProvider>
+        <Routes>
+          <Route path="/" element={<Login />} />
+          <Route
+            path="/dashboard/*"
+            element={
+              <ProtectedRoute>
+                <Dashboard />
+              </ProtectedRoute>
+            }
+          />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </AuthProvider>
       <Toaster position="top-right" reverseOrder={false} />
-    </>
+    </Router>
   );
 };
 
--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -1,5 +1,31 @@
+import { Routes, Route } from "react-router-dom";
+import Sidebar from "./Sidebar";
+import User from "./User";
+import Profile from "./Profile";
+import HeroPage from "../pages/HeroPage";
+
 const Dashboard = () => {
-  return <div>Dashboard</div>;
+  let year = new Date().getFullYear();
+
+  return (
+    <div className="min-h-screen bg-gray-50/50">
+      <Sidebar />
+      <div className="p-4 xl:ml-80">
+        <Routes>
+          <Route path="/user" element={<User />} />
+          <Route path="/ejemplo" element={<Profile />} />
+          <Route path="/" element={<HeroPage />} />
+        </Routes>
+        <div className="mt-12 text-blue-gray-600">
+          <footer className="py-2">
+            <p className="block antialiased text-center font-sans text-sm leading-normal font-normal text-inherit">
+              © {year}, Sistema Contable v1.0.
+            </p>
+          </footer>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Dashboard;
